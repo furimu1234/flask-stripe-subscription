0032b43acba674db5529bba56dfc4e18adb5bd85
furimu1234 <enu7693@gmail.com>
2022-12-23 06:46:47 +0900

    ボタンが押されたら決済画面を出す処理を追加

--- src/app.py
+++ src/app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+import os
+from dotenv import load_dotenv
+import stripe
 
 app = Flask(__name__)
 
+load_dotenv()
+
+PRICE_ID = os.getenv('PRICE_ID')
+ENDPOINT_SECRET = os.getenv("ENDPOINT_SECRET")
+
+stripe.api_key = os.getenv("SECRET_KEY")
+
 @app.get("/")
 def index():
-    return "OK"
+    return render_template("index.html")
+
+
+@app.get("/create-checkout-session")
+def create_checkout_session():
+    domain_url = "http://localhost/"
+    
+    checkout_session = stripe.checkout.Session.create(
+        success_url=domain_url + "success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url=domain_url + "cancel",
+        payment_method_types=["card"],
+        mode="subscription",
+        line_items=[
+            {
+                "price": PRICE_ID,
+                "quantity": 1,
+            }
+        ]
+    )
+    
+    return redirect(checkout_session.url)
+    
+
+
+    
 
 
 if __name__ == "__main__":
